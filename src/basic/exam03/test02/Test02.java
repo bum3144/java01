841885a1df6b4c7b7bf5892d3e23d41a8b5d4451
bit <bit@bitcompu-e7d62f.mshome.net>
2014-03-25 14:34:21 +0900

    .

---
+++ src/basic/exam03/test02/Test02.java
@@ -0,0 +1,9 @@
+package basic.exam03.test02;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+	
+	}
+	
+}
--- src/step01/exam03/test02/Test02.java
+++
@@ -1,9 +0,0 @@
-package step01.exam03.test02;
-
-public class Test02 {
-
-	public static void main(String[] args) {
-	
-	}
-	
-}
