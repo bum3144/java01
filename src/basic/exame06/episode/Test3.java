21c9db4d1c908ab76bdb3d8944f4433d02b6576c
bit <bit@bitcompu-e7d62f.mshome.net>
2014-04-07 19:34:15 +0900

    .

---
+++ src/basic/exame06/episode/Test3.java
@@ -0,0 +1,51 @@
+package basic.exame06.episode;
+
+import basic.exame06.episode.Collection3;
+
+public class Test3 {
+	static Collection3 coll = new Collection3();
+	
+	public static void testAdd() {
+			coll.add("홍길동");
+			coll.add("임꺽정");
+			coll.add("장보고");
+			coll.insert(2, "장길산");
+	}
+	
+	public static void testList() {
+		for (int i = 0; i < coll.size(); i++) {
+			System.out.println( coll.get(i) );
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			testAdd();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			testList();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/basic/exame06/jdbc/test01.java
+++ src/basic/exame06/jdbc/test01.java
@@ -1,9 +1,21 @@
 package basic.exame06.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class test01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// 1.드라이버 로딩 
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// 2.DB 커넥션 객체 가져오기
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://192.168.159.3:3306/studydb", "study", "study");
+		
+		System.out.println("오호라..연결되었다!");
+		
+		con.close();
 
 	}
 
